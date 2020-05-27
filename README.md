c40aac1bd743ba3abd80d735b61c1bf9e1da4681
acf-patrick <miharisoap@gmail.com>
2020-05-27 05:08:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tictactoe game #
+
+This game was made with python 3.4 and requiers at least
+Pygame version 1.9.2.
+You can choose to play with the computer.
+	
---
+++ morpion.py
@@ -0,0 +1,30 @@
+
+####################################################
+#Bibliotheque: PyGame 
+#Auteur: Tojoniaina Patrick 
+#Description: Simple jeu de morpion ecrit en Python
+####################################################
+
+# -*-coding:Latin-1 -*
+import pygame
+from pygame.locals import*
+from constante import*
+from classe import*
+
+moteur=Moteur()
+pygame.init()
+pygame.display.set_icon(pygame.image.load("data/icone.ico"))
+pygame.display.set_caption("Morpion 1.0")
+screen=pygame.display.set_mode((WIDTH,HEIGHT));
+done=False
+choix=MENU
+while not done:
+    if choix==MENU:
+        choix=moteur.menu(screen)
+    if choix==JEU:
+        choix=moteur.jeu(screen)
+    if choix==APRES_JEU:
+        choix=moteur.apres_jeu(screen)
+    if choix==QUITTER:
+        done=True
+pygame.quit()
